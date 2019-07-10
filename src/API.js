17198b89cb351aac4dd26ce0d6a51630897ad5ed
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2019-07-10 16:48:05 -0400

    Update and rename server.js to API.js

---
+++ src/API.js
@@ -0,0 +1,34 @@
+const wump = require('wumpfetch');
+
+module.exports = class API {
+  constructor (token) {
+    if (typeof token !== 'string') throw new SyntaxError('Invalid Token');
+    this.token = token
+  };
+
+  getBot(botid) {
+    if (typeof botid !== 'string') throw new TypeError('Bot ID must be a string');
+    return new Promise(async (resolve, reject) => {
+      try {
+         const res = await wump(`https://mythicalbots.xyz/api/bot/${botid}/info`).send();
+         resolve(res.json());
+      } catch (err) { reject(new Error(err)); }
+    });
+  }
+
+  postStats(servercount, botid) {
+    if (!this.token) throw new TypeError('API token not provided');
+    if (typeof servercount !== 'number') throw new TypeError('Server count must be a number');
+    if (typeof botid !== 'string') throw new TypeError('Bot ID must be a string');
+    return new Promise(async (resolve, reject) => {
+      try {
+         const res = await wump(`https://mythicalbots.xyz/api/bot/${botid}`, {
+          method: 'POST', 
+          headers: { 'Authorization': this.token },
+          data: { 'server_count': servercount  }
+        }).send();
+        resolve(res.json());
+      } catch (err) { reject(new Error(err)); }
+    });
+  }
+};
